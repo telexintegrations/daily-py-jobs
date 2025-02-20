0e9c44148369788d79a3ce7d800ec169decac48c
Ikechukwu Ugwuanyi <ik.ugwuanyi@gmail.com>
2025-02-20 11:34:50 +0100

    chore: move telex config json to public folder and serve as static file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import path from "path";
+
+const publicPath = path.join(__dirname, "public");
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
+app.use(express.static(publicPath));
 
 app.get("*", (req: Request, res: Response) => {
   res.send("Welcome to Daily Python Jobs");
