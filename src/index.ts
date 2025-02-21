dcea592b44df59bf04f04ae124f4e8e06029ea2c
Ikechukwu Ugwuanyi <ik.ugwuanyi@gmail.com>
2025-02-21 13:49:55 +0100

    chore: setup telex tick endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, Application } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import cors from "cors";
+import telexRoutes from "./telex/telex.route";
 
 const publicPath = path.join(__dirname, "public");
 
@@ -14,6 +15,8 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 
 app.use(express.static(publicPath));
+
+app.use("/telex", telexRoutes);
 
 app.get("*", (req: Request, res: Response) => {
   res.send("Welcome to Daily Python Jobs");
